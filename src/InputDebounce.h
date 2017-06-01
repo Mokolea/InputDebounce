5a2875d9e1d20946ccea040987ada3984471d1de
Mario Ban <mario.ban@bluewin.ch>
2017-06-01 14:38:15 +0200

    Clean-up

--- src/InputDebounce.h
+++ src/InputDebounce.h
@@ -5,7 +5,7 @@
   https://github.com/Mokolea/InputDebounce
 
 
-  Copyright 2015 Mario Ban
+  Copyright 2017 Mario Ban
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
